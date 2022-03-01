14f7fb8986a4d6f6285543fbbffb89cc23add12f
RapSku <raphaelskuza@freenet.de>
2022-03-01 13:51:06 +0100

    Improved documentation + small changes on imports

--- tfwda/model/tensorflow.py
+++ tfwda/model/tensorflow.py
@@ -1,26 +1,38 @@
-import tensorflow as tf
 import abc
-from abc import abstractmethod
+import tensorflow.python.keras.engine.functional
 
 
 class IFModel(metaclass=abc.ABCMeta):
+    """Interface for Model which holds the relevant information on
+    the neural network model/architecture
+    """
     pass
 
 
 class Model(IFModel):
-    """
-    Model holds all architecture relevant information, e.g. like summary,
-    weights and the architecture itself
-    Input:
-        - name          : str                                          -> Name of the architecture
-        - architecture  : tf.python.keras.engine.functional.Functional -> Keras/Tensorflow model
+    """Model holds all tensorflow architecture relevant information, e.g. like summary,
+    weights and the architecture structure itself
+
+    Parameters
+    ----------
+        name         : str
+            Name of the architecture
+        architecture : tf.python.keras.engine.functional.Functional
+            Tensorflow/Keras model
+
+    Attributes
+    ----------
+        summary : method
+            Prints the relevant structure of the architecture
+        weights : list
+            List of all layer variables and weights
     """
 
 
-    def __init__(self, name: str, architecture: tf.python.keras.engine.functional.Functional) -> None:
+    def __init__(self, name: str, architecture: tensorflow.python.keras.engine.functional.Functional) -> None:
         self.name         = name
-        if not isinstance(architecture, tf.python.keras.engine.functional.Functional):
+        if not isinstance(architecture, tensorflow.python.keras.engine.functional.Functional):
             raise TypeError('The architecture variable has to be of type tf.python.keras.engine.functional.Functional!')
         self.architecture = architecture
-        self.summary      = self.architecture.summary()
+        self.summary      = self.architecture.summary
         self.weights      = self.architecture.weights
--- tfwda/model_store/tensorflow.py
+++ tfwda/model_store/tensorflow.py
@@ -1,27 +1,38 @@
+import abc
 import tensorflow as tf
 import pymongo
-import abc
-from abc import ABCMeta, abstractmethod
-from pymongo import MongoClient
-from typing import List
-from collections import OrderedDict
+import collections
+from abc import abstractmethod
 
-from tfwda.model.tensorflow    import Model
-from tfwda.logger.standard     import Logger
-from tfwda.serializer.standard import Serializer
-from tfwda.plotter.standard    import Plotter
-from tfwda.utils.errors        import NotCreatedInstanceError
+import model.tensorflow 
+import logger.standard    
+import serializer.standard 
+import plotter.standard    
+import fitter.standard    
+import utils.errors   
 
 
-class IFModelStore(metaclass = ABCMeta):
+class IFModelStore(metaclass = abc.ABCMeta):
+    """Interface for every model store, the model store
+    coordinates all the communication between the components and
+    will command logging and database storage of important information
+
+    Methods (abstract)
+    ------------------
+        pipe_models(models list[model.tensorflow.IFModel])
+            A list of models will be piped into the model store and the model
+            store will delegate these models to the different components
+        get_instance(db_connection_string str, database_name str) IFModelStore
+            Instance of the model store is either initialised or fetched
+    """
     @abstractmethod
-    def pipe_models(self, models : List[Model]):
+    def pipe_models(self, models: list[model.tensorflow.IFModel]):
         pass
 
     
     @staticmethod
     @abstractmethod
-    def get_instance(db_connection_string : str, database_name : str):
+    def get_instance(db_connection_string: str, database_name: str) -> object:
         pass
 
 
@@ -32,13 +43,37 @@ class ModelStore(IFModelStore):
     is represents the central point of model traffic. The fitting, plotting and 
     serializing are all controlled by the model store.
 
-    Input:
-        - db_connection_string : str  -> Database Connection String
-        - database_name        : str  -> The name of the database of interest
-        - path_to_dir          : str  -> The Path to the directory where the plots should be deposited
-        - verbosity            : bool -> The verbosity of the information which are given to the user
-    """
+    Parameters
+    ----------
+        db_connection_string : str
+            Database Connection String for MongoDB
+        database_name        : str
+            The name of the database of interest
+        path_to_dir          : str
+            The Path to the directory where the plots should be deposited
+        verbosity            : bool
+            The verbosity of the information which are given to the user over console
 
+    Attributes
+    ----------
+        logger     : logger.standard.Logger
+            Logging instance which logs all the information to the console
+        serializer : serializer.standard.Serializer
+            Serializer instance which is responsible for flattening the neural network model
+        plotter    : plotter.standard.Plotter
+            Plotting instance which will perform all the distribution plots
+        fitter     : fitter.standard.Fitter
+            Fitting instance which will fit different distribution if the user would like
+            to perform a fit on given weight distributions
+
+    Methods
+    -------
+        get_instance(db_connection_string str, database_name str, path_to_dir str, verbosity bool) ModelStore `staticmethod`
+            Since the model store is a Singleton, this method must be use to fetch an instance, this will be
+            the central instance, coordinating and speaking to all the other components
+        pipe_models(models list[model.tensorflow.Model])
+            A list of tensorflow models will be serialized, plotted and fitted
+    """
     __instance = None
     __client   = None
     __database = None
@@ -49,50 +84,79 @@ class ModelStore(IFModelStore):
 
     def __init__(self, db_connection_string: str, database_name: str, path_to_dir: str, verbosity: bool) -> None:
         if ModelStore.__instance != None:
-            raise NotCreatedInstanceError("An instance has been already created! Get it with ModelStore.get_instance()!")
-        else:
-            ModelStore.__instance = self
+            raise utils.errors.NotCreatedInstanceError("An instance has been already created! Get it with ModelStore.get_instance()!")
+        ModelStore.__instance = self
         if self.__database == None:
             self.__setup_db_connection(db_connection_string, database_name)
-            self.logger     = Logger(verbosity)
-            self.serializer = Serializer(self.logger)
-            self.plotter    = Plotter(self.logger, path_to_dir)
+            self.logger     = logger.standard.Logger(verbosity)
+            self.serializer = serializer.standard.Serializer(self.logger)
+            self.plotter    = plotter.standard.Plotter(self.logger, path_to_dir)
+            self.fitter     = fitter.standard.Fitter(self.plotter, self.logger)
 
 
     @staticmethod
     def get_instance(db_connection_string: str, database_name: str, path_to_dir: str, verbosity: bool) -> object:
-        """ This class is a Singleton, thus the instance is returned through this method. """
+        """This class is a Singleton, thus the instance is returned through this method
+        
+        Parameters
+        ----------
+            db_connection_string : str
+                MongoDB connection string
+            database_name        : str
+                Name of the target database
+            path_to_dir          : str
+                Path where the plots will be stored in
+            verbosity            : bool
+                Whether the info output to the console should be verbose or not
+
+        Returns
+        -------
+            ModelStore
+                Instance of the ModelStore class
+        """
         if ModelStore.__instance == None:
             ModelStore(db_connection_string, database_name, path_to_dir, verbosity)
         return ModelStore.__instance
 
 
-    def pipe_models(self, models: List[Model]) -> None:
-        """ 
-        A list of models is processed, meaning that weights are extracted, given to the Serializer,
+    def pipe_models(self, models: list[model.tensorflow.Model]) -> None:
+        """A list of models is processed, meaning that weights are extracted, given to the serializer,
         metadata are written and the plots are generated 
+
+        Parameters
+        ----------
+            models : list[model.tensorflow.Model]
+                A list of tensorflow models which weights should be analysed
         """
         self.logger.log(f"{len(models)} models are being processed now!", "Header")
-
-        count = 0
-        serialized_weights_per_model = OrderedDict()
-        for model in models:
-            count += 1
-            flattened_weights, metadata = self.serializer.flatten(model)
+        serialized_weights_per_model = collections.OrderedDict()
+        for count, model in enumerate(models):
+            flattened_weights, metadata              = self.serializer.flatten(model)
             serialized_weights_per_model[model.name] = {'weights': flattened_weights, 'metadata': metadata}
             self.logger.log(f"{count} model serializations have been finished...", "Info")
-
         self.logger.log(f"{len(models)} models have been processed now...", "Info")
-        self.logger.log("Plotting will start now!", "Header")
 
+        self.logger.log("Plotting will start now!", "Header")
         for model_name, model_data in serialized_weights_per_model.items():
             self.plotter.plot(model_name, model_data["weights"], model_data["metadata"])
+        self.logger.log("Plotting terminated successfully!", "Info")
+
+        self.logger.log("Fitting process will start now!", "Header")
+        for model_name, model_data in serialized_weights_per_model.items():
+            self.fitter.fit(model_data)
+        self.logger.log("Fitting process terminated successfully!", "Info")
 
 
     def __setup_db_connection(self, db_connection_string: str, database_name: str) -> None:
-        """ 
-        This method setups the database connection - pay attention: this method does
-        not tell you if a successful connection could be established. 
+        """This method setups the database connection - pay attention: this method does
+        not check whether a successful connection could be established!
+
+        Parameters
+        ----------
+            db_connection_string : str
+                MongoDB connection string
+            database_name        : str
+                Name of the target database
         """
-        self.__client   = MongoClient(db_connection_string)
+        self.__client   = pymongo.MongoClient(db_connection_string)
         self.__database = self.__client[database_name]
